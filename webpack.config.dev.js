9d6d52daf921516bd0ae5abe4b376225722305f3
Faiz Shaikh <faiz.shaikh@hotmail.com>
2017-10-24 16:41:00 +0100

    Webpack Bundling added

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+        debug: true,
+        noInfo: false,
+      })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+    ]
+  }
+}
